5bec44cfb179c8a1d4b7979f742c41298d0a4e3e
faridlamaul <lamaulfarid71@gmail.com>
2023-02-20 12:02:29 +0700

    initialize java project

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import org.cloudbus.cloudsim.examples.CloudSimExample1;
+import org.cloudbus.cloudsim.examples.CloudSimExample2;
+import org.cloudbus.cloudsim.examples.CloudSimExample3;
+import org.cloudbus.cloudsim.examples.CloudSimExample4;
+import org.cloudbus.cloudsim.examples.CloudSimExample5;
+import org.cloudbus.cloudsim.examples.CloudSimExample6;
+
+public class Main {
+    public static void main(String[] args) {
+        // CloudSimExample1.main(args);
+         CloudSimExample2.main(args);
+        // CloudSimExample3.main(args);
+        // CloudSimExample4.main(args);
+        // CloudSimExample5.main(args);
+        // CloudSimExample6.main(args);
+    }
+}
